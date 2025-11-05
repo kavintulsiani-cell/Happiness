a3c040a5a3ae8871be55e9e40daf90cd2902c490
Kavin Tulsiani <kavintulsiani@gmail.com>
2025-11-05 13:28:34 +0530

    Program complete

--- happiness.py
+++ happiness.py
@@ -3,10 +3,13 @@ import plotly.express as ps
 
 
 st.title = ("In Search for Happiness")
-selection1 = st.selectbox  ("Select the data for the X-axis", "GDP", "Generosity", "Happiness")
-selection2 = st.selectbox  ("Select the data for the Y-axis", "GDP", "Generosity", "Happiness")
-st.subheader = {selection1} and {selection2}
+selection1 = st.selectbox  ("Select the data for the X-axis", ("GDP", "Generosity", "Happiness")).lower()
+selection2 = st.selectbox  ("Select the data for the Y-axis", ("GDP", "Generosity", "Happiness")).lower()
+st.subheader (f"{selection1.upper()} and {selection2.upper()}")
 
-
-fig = px.scatter (x = selection1, y = selection2)
-st.plotly_chart(fig)
+import pandas as pd
+df = pd.read_csv ("happy.csv")
+if selection1 and selection2 :
+    fig = ps.scatter (data_frame = df, x = selection1, y = selection2,
+                  labels = {selection1 :selection1.upper(), selection2 : selection2.upper()})
+    st.plotly_chart(fig)
